9d7fd7fa7acbfcedb2dc4b27e3a382d871562526
chikara <chenhongtu@51xianqu.net>
2017-02-25 15:39:22 +0800

    refactor: strip classexpression

--- src/index.js
+++ src/index.js
@@ -4,68 +4,62 @@ import {isEventAttribute, isHandlerExpression, isHandlerExpressionWithParams, is
 function plugin({types: t}) {
   return {
     visitor: {
-      Program(programPath, {opts}) {
+      ClassDeclaration(classPath, {opts}) {
         const defaultOpts = {
-          propPrefix: 'on',
-          advanced: false
-        }
+            propPrefix: 'on',
+            advanced: false
+          },
+          superClass = classPath.get('superClass'),
+          addedEventNames = []
+        let bodyPaths = classPath.get('body').get('body')
 
         opts = {...defaultOpts, ...opts}
-        programPath.traverse({
-          ClassExpression(classPath) {
-            const superClass = classPath.get('superClass'),
-              addedEventNames = []
-            let bodyPaths = classPath.get('body').get('body')
+        if (!superClass || !isReactClass(superClass, classPath.scope)) {
+          return
+        }
+        if (!bodyPaths.some(bodyPath =>
+          bodyPath.isClassMethod({kind: 'constructor'})
+        )) {
+          classPath.get('body').node.body.unshift(buildConstructor(t))
+          bodyPaths = classPath.get('body').get('body')
+        }
+        classPath.traverse({
+          JSXAttribute(path) {
+            let eventName,
+              JSXExpression
 
-            if (!superClass || !isReactClass(superClass, classPath.scope)) {
+            if (!isEventAttribute(path, opts.propPrefix)) {
               return
             }
-            if (!bodyPaths.some(bodyPath =>
-              bodyPath.isClassMethod({kind: 'constructor'})
-            )) {
-              classPath.get('body').node.body.unshift(buildConstructor(t))
-              bodyPaths = classPath.get('body').get('body')
+            JSXExpression = path.get('value').get('expression')
+            if (opts.advanced === true && isHandlerExpressionWithParams(JSXExpression, t)) {
+              const handler = JSXExpression.get('callee').get('property').node.name,
+                nodeArgs = JSXExpression.node.arguments
+
+              JSXExpression.replaceWith(buildArrowFunction(t, handler, nodeArgs))
+
+              return
+            }
+            if (!isHandlerExpression(path.get('value').get('expression'), t)) {
+              return
             }
 
-            classPath.traverse({
-              JSXAttribute(path) {
-                let eventName,
-                  JSXExpression
-
-                if (!isEventAttribute(path, opts.propPrefix)) {
-                  return
+            eventName = JSXExpression.get('property').node.name
+            if (~this.addedEventNames.indexOf(eventName)) {
+              return
+            }
+            if (bodyPaths.some(bodyPath =>
+              bodyPath.get('key').isIdentifier({name: eventName})
+            )) {
+              bodyPaths.forEach(bodyPath => {
+                if (bodyPath.isClassMethod({kind: 'constructor'})) {
+                  bodyPath.get('body').pushContainer('body', buildBindingStatement(t, eventName))
+                  this.addedEventNames.push(eventName)
                 }
-                JSXExpression = path.get('value').get('expression')
-                if (opts.advanced === true && isHandlerExpressionWithParams(JSXExpression, t)) {
-                  const handler = JSXExpression.get('callee').get('property').node.name,
-                    nodeArgs = JSXExpression.node.arguments
-
-                  JSXExpression.replaceWith(buildArrowFunction(t, handler, nodeArgs))
-
-                  return
-                }
-                if (!isHandlerExpression(path.get('value').get('expression'), t)) {
-                  return
-                }
-
-                eventName = JSXExpression.get('property').node.name
-                if (~this.addedEventNames.indexOf(eventName)) {
-                  return
-                }
-                if (bodyPaths.some(bodyPath =>
-                  bodyPath.get('key').isIdentifier({name: eventName})
-                )) {
-                  bodyPaths.forEach(bodyPath => {
-                    if (bodyPath.isClassMethod({kind: 'constructor'})) {
-                      bodyPath.get('body').pushContainer('body', buildBindingStatement(t, eventName))
-                      this.addedEventNames.push(eventName)
-                    }
-                  })
-                }
-              }
-            }, {addedEventNames})
+              })
+            }
           }
-        })
+        }, {addedEventNames})
       }
     }
   }
