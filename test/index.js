0c5294c45440f8d551d6f88c549ac50db6408f08
chikara <chenhongtu@51xianqu.net>
2017-03-27 17:45:06 +0800

    test: add test entry point in dirname loop

--- test/index.js
+++ test/index.js
@@ -1,2 +1,31 @@
-// Due to business busy
-// Test case is written in future
+import test from 'ava'
+import { promisify } from 'bluebird'
+import { readdirSync, readFile } from 'fs'
+import path from 'path'
+import { transformFile } from 'babel-core'
+import plugin from '../src'
+
+const dirnames = readdirSync(path.resolve(__dirname, 'fixtures'))
+
+dirnames.forEach(dirname => {
+  test(`fixtures/${dirname}`, async t => {
+    const [{ code }, expectedCode] = await Promise.all([
+      promisify(transformFile)(
+        path.resolve(__dirname, `fixtures/${dirname}/actual.js`),
+        {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: [plugin]
+        }
+      ),
+      promisify(readFile)(
+        path.resolve(__dirname, `fixtures/${dirname}/expected.js`),
+        'utf-8'
+      )
+    ])
+
+    t.is(code, expectedCode)
+  })
+})
+
+
+
