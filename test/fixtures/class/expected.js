fadab2e953844f6ddf7245626e2d42ebedada15d
chikara <chenhongtu@51xianqu.net>
2017-03-28 10:53:47 +0800

    test: test case for generic class

--- test/fixtures/class/expected.js
+++ test/fixtures/class/expected.js
@@ -1,32 +1,27 @@
-'use strict'
+'use strict';
 
-let _classCallCheck2 = require('babel-runtime/helpers/classCallCheck')
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
 
-let _classCallCheck3 = _interopRequireDefault(_classCallCheck2)
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-let _createClass2 = require('babel-runtime/helpers/createClass')
+var _createClass2 = require('babel-runtime/helpers/createClass');
 
-let _createClass3 = _interopRequireDefault(_createClass2)
+var _createClass3 = _interopRequireDefault(_createClass2);
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : {
-    default: obj
-  }
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-let A = function() {
+var A = function () {
   function A() {
-    (0, _classCallCheck3.default)(this, A)
+    (0, _classCallCheck3.default)(this, A);
 
-    this.a = 1
+    this.a = 1;
   }
 
   (0, _createClass3.default)(A, [{
     key: 'say',
     value: function say() {
-      return 'hello'
+      return 'hello';
     }
-  }])
-
-  return A
-}()
+  }]);
+  return A;
+}();
